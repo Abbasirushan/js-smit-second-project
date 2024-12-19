f79a01b8d73812b12577c4b547e112144fd081b2
rushan abbasi <143517351+Abbasirushan@users.noreply.github.com>
2024-12-20 00:23:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,230 @@
+// question-no-1
+// Declare 3 variables in one statement.
+
+// var p = 5, q = 6, r = 7;
+
+// question-no-2
+// Declare 5 legal & 5 illegal variable names.
+
+//this legal variable 
+
+//let myVariable = "This is legal";
+//let _privateVariable = "This is also legal";
+//let $dollarSign = "Dollar sign is valid";
+//let user123 = "Numbers are fine as long as not at the start";
+//let camelCaseVariable = "This follows common practice";
+
+//this is illegal variable 
+
+//let var = "Reserved keyword";             
+//let my-variable = "Contains a hyphen";    
+//let @username = "Starts with @";          
+//let let myVar = "Contains a space
+
+
+//question-no-3
+//Display this in your browser
+
+//  const heading = document.createElement("h1");
+// heading.textContent = "Rules for Naming JS Variables";
+// document.body.appendChild(heading);
+
+// const rule1 = document.createElement("p");
+// rule1.innerHTML = "Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>underscores (_)</b>, and <b>dollar signs ($)</b>.<br>For example: <code>$my_1stVariable</code>";
+// document.body.appendChild(rule1);
+
+
+// const rule2 = document.createElement("p");
+// rule2.innerHTML = "Variables must begin with a <b>letter</b>, <b>underscore (_)</b>, or <b>dollar sign ($)</b>.<br>For example: <code>$name</code>, <code>_name</code>, or <code>name</code>";
+// document.body.appendChild(rule2);
+
+
+// const rule3 = document.createElement("p");
+// rule3.innerHTML = "Variable names are case <b>sensitive</b>.";
+// document.body.appendChild(rule3);
+
+// const rule4 = document.createElement("p");
+// rule4.innerHTML = "Variable names should not be JS <b>reserved keywords</b>.";
+// document.body.appendChild(rule4);
+
+                                         //...end...//
+
+
+
+//question-no-1
+//Repeat task1 for subtraction, multiplication, division &modulus. 
+
+// function performOperations(a, b) {
+//     let subtraction = a - b;
+//     console.log(`Subtraction: ${a} - ${b} = ${subtraction}`);
+
+//     let multiplication = a * b;
+//     console.log(`Multiplication: ${a} * ${b} = ${multiplication}`);
+
+//     let division = a / b;
+//     console.log(`Division: ${a} / ${b} = ${division}`);
+
+//     let modulus = a % b;
+//     console.log(`Modulus: ${a} % ${b} = ${modulus}`);
+// }
+
+// let num1 = 20;
+// let num2 = 6;
+// performOperations(num1, num2);
+
+                                      
+//question-no-2
+
+// Do the following using JS Mathematic Expressions
+
+// let myVariable;
+
+// console.log("Value after variable declaration is: " + myVariable);
+
+// myVariable = 5;
+
+// console.log("Initial value: " + myVariable);
+
+// myVariable++;
+
+// console.log("Value after increment is: " + myVariable);
+
+// myVariable += 7;
+
+// console.log("Value after addition is: " + myVariable);
+
+// myVariable--;
+
+// console.log("Value after decrement is: " + myVariable);
+
+// let remainder = myVariable % 3;
+
+// console.log("The remainder is: " + remainder);
+
+
+//qiuestion-no-3
+
+
+// let ticketPrice = 600;
+
+// let totalCost = ticketPrice * 5;
+
+// console.log("The cost of buying 5 movie tickets is: " + totalCost + " PKR");
+
+
+// question-no-4
+
+
+// let number = parseInt(prompt("Enter a number to display its multiplication table:"));
+
+// console.log(`Multiplication Table of ${number}:`);
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} x ${i} = ${number * i}`);
+// }
+
+
+
+//question-no-5
+
+
+// let celsiusTemperature = 30;
+
+// let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// console.log(`${celsiusTemperature}°C is ${fahrenheitTemperature}°F`);
+
+// fahrenheitTemperature = 86;
+
+// celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+// console.log(`${fahrenheitTemperature}°F is ${celsiusTemperature.toFixed(2)}°C`);
+
+
+//question-no-6
+
+// let priceOfItem1 = 500;
+
+// let priceOfItem2 = 300;
+
+// let quantityOfItem1 = 2;
+
+// let quantityOfItem2 = 3;
+
+// let shippingCharges = 100;
+
+// let totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// console.log("Receipt:");
+// console.log(`Price of item 1: ${priceOfItem1}`);
+// console.log(`Quantity of item 1: ${quantityOfItem1}`);
+// console.log(`Price of item 2: ${priceOfItem2}`);
+// console.log(`Quantity of item 2: ${quantityOfItem2}`);
+// console.log(`Shipping charges: ${shippingCharges}`);
+// console.log(`Total cost: ${totalCost}`);
+
+
+//question-no-7
+
+// let totalMarks = 500; 
+// let marksObtained = 375; 
+
+// let percentage = (marksObtained / totalMarks) * 100;
+
+// console.log("Result:");
+// console.log(`Total Marks: ${totalMarks}`);
+// console.log(`Marks Obtained: ${marksObtained}`);
+// console.log(`Percentage: ${percentage.toFixed(2)}%`);
+
+//question-no-8
+
+
+// const exchangeRateUSDToPKR = 104.80; 
+// const exchangeRateSARToPKR = 28; 
+
+// let dollars = 10;
+// let riyals = 25;
+
+// let totalPKR = (dollars * exchangeRateUSDToPKR) + (riyals * exchangeRateSARToPKR);
+
+// console.log(`Total currency in Pakistani Rupees: ${totalPKR} PKR`);
+
+
+//question-no-9
+
+
+// let number = 20; 
+
+// let result = ((number + 5) * 10) / 2;
+
+// console.log(`The result of the arithmetic operations is: ${result}`);
+
+
+//question-no-10
+
+
+// let currentYear = 2024; 
+// let birthYear = 1990; 
+
+// let age1 = currentYear - birthYear;
+// let age2 = age1 - 1; 
+// console.log(`They are either ${age2} or ${age1} years old.`);
+
+
+// question-no-11
+
+
+// let radius = 7; 
+
+// const pi = 3.142;
+
+// let circumference = 2 * pi * radius;
+// console.log(`The circumference is ${circumference}`);
+
+// let area = pi * radius * radius;
+// console.log(`The area is ${area}`);
+
+
+
+//end second assgimenets
+
+
+//question-on-1
+
